b2a9ae37b53691a9382c34e5620f315e65b7c73e
SantiH <144285440+Chico-son@users.noreply.github.com>
2024-10-08 12:25:13 -0400

    Update Harmonic_Client.java
    Uses JavaMath.java "hypotenuse" function
    instead of the Harmonic.java "Harmonic" function.

--- Harmonic_Client.java
+++ Harmonic_Client.java
@@ -4,7 +4,7 @@ public class Harmonic_Client{
             int arg = Integer.parseInt(args[i]);
             double value = Harmonic.harmonic(arg);
             StdOut.println(value);
-            StdOut.println(Harmonic.hypotenuse(value, value + 3));
+            StdOut.println(JavaMath.hypotenuse(value, value + 3));
         }
     }
 }
